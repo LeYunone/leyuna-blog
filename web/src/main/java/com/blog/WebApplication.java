2467f1d6584bed7a097b2b1a77dd3847645f510c
leyunone <365627310@qq.com>
2021-09-03 16:33:38 +0800

    [update]packing

--- web/src/main/java/com/blog/WebApplication.java
+++ web/src/main/java/com/blog/WebApplication.java
@@ -2,15 +2,21 @@ package com.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication()
 @EnableCaching //开启缓存
 //@ComponentScan({"com.blog.control","com.blog.api","com.blog.daoservice"})
-public class WebApplication {
+public class WebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WebApplication.class);
+    }
 }
